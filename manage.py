b799b934b0f9bd0f3aba5b58a2b2e68e0b26aff4
Eltrobit <104744765+Eltrobit@users.noreply.github.com>
2025-02-04 20:03:56 +1000

    I always come back

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+#Tu@Ya@Tu@Ya@Tu@Ya@Tu@Ya@Tu@Ya@Tu@Ya@Tu@Ya@Tu@Ya@Tu@Ya@Tu@Ya@
 
 def main():
     """Run administrative tasks."""
